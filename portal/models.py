3ccaf8536d4d9aa668eb1bf86bb2e8f6edff008b
Anderson Gomes <anderson.gomes@tompast.com.br>
2018-07-31 15:45:10 -0300

    add models

---
+++ portal/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=30,
+        blank=True
+    )
+    slug = models.SlugField(
+        unique=True
+    )
+    parent = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        related_name='cat_child',
+        on_delete=models.PROTECT
+    )
+    orden = models.IntegerField(
+        null=True,
+        blank=True
+    )
+    hidden = models.BooleanField(
+        default=False
+    )
+
+    class Meta:
+            verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=30,
+        blank=True
+    )
+    slug = models.SlugField(
+        unique=True
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.PROTECT
+    )
+    Categories = models.ManyToManyField(
+        Category,
+        blank=True,
+        related_name='categories',
+    )
+    quantity = models.IntegerField(
+        default=1,
+
+    )
+    prince = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+    short_description = models.CharField(
+        max_length=255
+    )
+    description = models.TextField(
+        null=True,
+        blank=True
+    )
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('Inactive', 'Inactive')
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="Inactive"
+    )
+
+    class meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
