9689947b098ae9ccc95e6da3a0e9fe0194a2b5c1
Anderson Gomes <anderson.gomes@tompast.com.br>
2018-08-16 16:44:41 -0300

    create logout

--- login/urls.py
+++ login/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path(r'register/success', views.register_success, name='login_register_success'),
     path(r'register', views.register, name='login_register'),
     path(r'login/', auth_views.login, name='login'),
+    path(r'logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+
 ]
