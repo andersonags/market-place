7b898b7a8fd0d51e5c9bf21db70454c040cd640e
Anderson Gomes <anderson.gomes@tompast.com.br>
2018-08-16 16:37:07 -0300

    Create template Login

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path(r'register/success', views.register_success, name='login_register_success'),
     path(r'register', views.register, name='login_register'),
+    path(r'login/', auth_views.login, name='login'),
 ]
