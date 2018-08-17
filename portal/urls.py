e16ce92a19193265e51dea8ebf2c812b02b741ba
Anderson Gomes <anderson.gomes@tompast.com.br>
2018-08-17 15:32:11 -0300

    add template in products_new.html and add products_new route

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path(r'', views.home, name='home'),
     path(r'my_products', views.my_products, name='my_products'),
+    path(r'products/new', views.products_new, name='products_new'),
 ]
