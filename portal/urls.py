59569dd9646c02f6a8c7d94fbf2012a3fb6e3cf0
Anderson Gomes <anderson.gomes@tompast.com.br>
2018-08-17 15:33:20 -0300

    Correction in route

--- portal/urls.py
+++ portal/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path(r'', views.home, name='home'),
     path(r'my_products', views.my_products, name='my_products'),
-    path(r'products/new', views.products_new, name='products_new'),
+    path(r'product/new', views.product_new, name='product_new'),
 ]
